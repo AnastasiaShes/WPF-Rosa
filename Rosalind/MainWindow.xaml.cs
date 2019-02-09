46d611fe215a36c6d0843cf2ee43c448e25a296a
Anastasia <shestopalova-nv@mail.ru>
2019-02-09 13:38:09 +0300

    Незначительное изменение

--- Rosalind/MainWindow.xaml.cs
+++ Rosalind/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
@@ -20,34 +11,37 @@ using System.Collections.Specialized;
 namespace Rosalind
 {
 
-    /// Для грида полосы: ShowGridLines="True"
-
- public partial class MainWindow : Window
+    public partial class MainWindow : Window
     {
-        string connectionString;
-
         public MainWindow()
         {
-            InitializeComponent();
 
-            FillingLB(); //Заполнение списка
+           InitializeComponent();
+
+           FillingLB(); //Заполнение списка
 
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) //Выбор ячейки
         {
-            MessageBox.Show(listBox.SelectedItem.ToString()); 
+            if (listBox.SelectedItem is Information _info)
+            {
+                TB_1.Text = _info.Name;
+                Tb_id.Text = _info.Index;
+                TB_Condition.Text = "Условие: " +  _info.Condition;
+                EXP_text.Text = _info.Text;
+            }            
         }
 
         private void FillingLB() //Метод для заполнения списка
         {
-            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
             var Info = new List<Information>();
 
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
-                using (SqlCommand command = new SqlCommand("SELECT [Id], [Name] FROM [Information]", sqlConnection))
+                using (SqlCommand command = new SqlCommand("SELECT * FROM [Information]", sqlConnection))
                 {
                     DataTable dt = new DataTable();
                     sqlConnection.Open();
@@ -56,38 +50,14 @@ namespace Rosalind
                     {
                         dt.Load(reader);
 
-                        Info = dt.AsEnumerable().Select(se => new Information(se.Field<string>("Name"), se.Field<string>("Id")) { Name = se.Field<string>("Id"), Index = se.Field<string>("Name") }).ToList();
+                        Info = dt.AsEnumerable().Select(se => new Information(se.Field<string>("Name"), se.Field<string>("Id"), se.Field<string>("Condition"), se.Field<string>("Text")) { Name = se.Field<string>("Id"), Index = se.Field<string>("Name"), Condition = se.Field<string>("Condition"), Text = se.Field<string>("Text") }).ToList();
                     }
                 }
             }
-
             listBox.ItemsSource = Info;
         }
     }
 }
 
-public class Information //Класс для заполнение коллекции
-{
-    private readonly string name;
-    private readonly string index;
-
-    public Information(string name, string index)
-    {
-        this.name = name;
-        this.index = index;
-    }
-
-    public string Name
-    {
-        get { return name; }
-        set { }
-    }
-
-    public string Index
-    {
-        get { return index; }
-        set { }
-    }
-}
 
 
---
+++ Rosalind/logic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+class logic
+{
+    
+}
+
+public class Information
+{
+    private readonly string name;
+    private readonly string index;
+    private readonly string condition;
+    private readonly string text;
+
+    public Information(string name, string index, string condition, string text)
+    {
+        this.name = name;
+        this.index = index;
+        this.condition = condition;
+        this.text = text;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { }
+    }
+
+    public string Index
+    {
+        get { return index; }
+        set { }
+    }
+
+    public string Condition
+    {
+        get { return condition; }
+        set { }
+    }
+
+    public string Text
+    {
+        get { return text; }
+        set { }
+    }
+}
